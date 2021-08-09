671594fbc303b22078c9c70c94764fc791e76134
Daniil <56131760+Daniil-Kienko@users.noreply.github.com>
2021-08-09 14:02:43 +0300

    Added basic functionality

---
+++ BotAlert/Controllers/BaseController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace BotAlert.Controllers
+{
+
+    public class BaseController
+    {
+
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var handler = update.Type switch
+            {
+                // UpdateType.Unknown:
+                // UpdateType.ChannelPost:
+                // UpdateType.EditedChannelPost:
+                // UpdateType.ShippingQuery:
+                // UpdateType.PreCheckoutQuery:
+                // UpdateType.Poll:
+                UpdateType.Message => BotOnMessageReceived(botClient, update.Message),
+                // UpdateType.EditedMessage => BotOnMessageReceived(botClient, update.EditedMessage),
+                // UpdateType.CallbackQuery => BotOnCallbackQueryReceived(botClient, update.CallbackQuery),
+                // UpdateType.InlineQuery => BotOnInlineQueryReceived(botClient, update.InlineQuery),
+                // UpdateType.ChosenInlineResult => BotOnChosenInlineResultReceived(botClient, update.ChosenInlineResult),
+                _ => UnknownUpdateHandlerAsync(botClient, update)
+            };
+
+            try
+            {
+                await handler;
+            }
+            catch (Exception exception)
+            {
+                await HandleErrorAsync(botClient, exception, cancellationToken);
+            }
+        }
+
+        private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
+        {
+            if (message.Type != MessageType.Text)
+                return;
+
+            switch (message.Text)
+            {
+                case "/create":
+                    //CreateNotification(botClient, message);
+                    break;
+                case "/get_notifications":
+                    //GetAllNotifications(botClient, message);
+                    break;
+                case "/get_notification":
+                    //GetNotification(botClient, message);
+                    break;
+                case "/edit":
+                    //EditNotification(botClient, message);
+                    break;
+                case "/delete":
+                    //DeleteNotification(botClient, message);
+                    break;
+                default:
+                    //await HandleInputError(botClient, message);
+                    break;
+            }
+
+            botClient.SendTextMessageAsync(message.Chat.Id, message.Text);
+        }
+
+        private static Task UnknownUpdateHandlerAsync(ITelegramBotClient botClient, Update update)
+        {
+            botClient.SendTextMessageAsync(update.Message.Chat.Id, "Sorry, something went worng, please try again later!");
+            return Task.CompletedTask;
+        }
+
+        public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
--- BotAlert/Program.cs
+++ BotAlert/Program.cs
@@ -1,13 +1,39 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BotAlert.Controllers;
+using BotAlert.Settings;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+using Telegram.Bot.Extensions.Polling;
 
 namespace BotAlert
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static TelegramBotClient? Bot;
+
+        public static async Task Main()
         {
-            CreateHostBuilder(args).Build().Run();
+            Bot = new TelegramBotClient(Settings.Settings.ConnectionString);
+
+            var me = await Bot.GetMeAsync();
+            Console.Title = me.Username;
+
+            using var cts = new CancellationTokenSource();
+
+            // StartReceiving does not block the caller thread. Receiving is done on the ThreadPool.
+            Bot.StartReceiving(new DefaultUpdateHandler(BaseController.HandleUpdateAsync, BaseController.HandleErrorAsync),
+                               cts.Token);
+
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.ReadLine();
+
+            // Send cancellation request to stop bot
+            cts.Cancel();
+
+            // CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
