b92c66cc2e31d1ba190120585437107ace215a35
Daniil <56131760+Daniil-Kienko@users.noreply.github.com>
2021-08-10 10:25:08 +0300

    Editeed .gitignore file and added settings

---
+++ BotAlert/Settings/DBSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotAlert.Settings
+{
+    public class DBSettings
+    {
+        public const string ConfigKey = "MongoDBSettings";
+
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ BotAlert/Settings/TelegramSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BotAlert.Settings
+{
+    public class TelegramSettings
+    {
+        public const string ConfigKey = "TelegramSettings";
+
+        public string BotApiKey { get; set; }
+    }
+}
