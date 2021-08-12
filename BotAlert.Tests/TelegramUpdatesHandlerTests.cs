00f7d1c87af3a5fc49ad103b275f35ffda383b20
Anton Artomov <86731073+AntonArtomovNM@users.noreply.github.com>
2021-08-12 15:42:48 +0300

    Added tests for EventProvider, InputDateState, InputWarnDateState, InputWarnDateKeyboardState, InputDescriptionKeyboardState and base for TelegramUpdatesHandler tests

---
+++ BotAlert.Tests/TelegramUpdatesHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using BotAlert.Controllers;
+using BotAlert.Interfaces;
+using FakeItEasy;
+using Xunit;
+
+namespace BotAlert.Tests
+{
+    public class TelegramUpdatesHandlerTests
+    {
+        private readonly IStateProvider _stateProviderMock;
+        private readonly IStateFactory _stateFactoryMock;
+
+        private readonly TelegramUpdatesHandler _updatesHandler;
+
+        public TelegramUpdatesHandlerTests()
+        {
+            _stateFactoryMock = A.Fake<IStateFactory>();
+            _stateProviderMock = A.Fake<IStateProvider>();
+
+            _updatesHandler = new TelegramUpdatesHandler(_stateProviderMock, _stateFactoryMock);
+        }
+
+    }
+}
